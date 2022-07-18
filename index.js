2e51e347c6e444ca5efdfdeaada117a0d9755add
Vladimir Mkhitaryan <v.mkhitaryan@datarockets.com>
2022-07-18 20:50:57 +0400

    Add eslint configs
    Fix index.js, admin-user-router eslint issue

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const env = require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-
-
 const express = require('express');
 const app = express();
 const http = require('http');
-const nunjucks = require('nunjucks');
 const server = http.createServer(app);
 
 const mongoose = require('mongoose');
 
-( async () =>{
-   await mongoose.connect('mongodb://localhost:27017/lets-guess');
-  })();
-
+(async () => {
+    await mongoose.connect('mongodb://localhost:27017/lets-guess');
+})();
 
 const cookieParser = require('cookie-parser');
 const config = require('./server/config');
@@ -27,16 +23,12 @@ const { Strategy } = require('passport-jwt');
 
 const { jwt } = require('./server/config');
 
-passport.use(
-  new Strategy(jwt, function(jwt_payload, done) {
-    if (jwt_payload != void 0) {
-      return done(false, jwt_payload);
+passport.use(new Strategy(jwt, ((jwt_payload, done) => {
+    if (jwt_payload !== void 0) {
+        return done(false, jwt_payload);
     }
     done();
-  })
-);
-
-
+})));
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -46,8 +38,7 @@ app.use(cookieParser());
 
 require('./server/admin-user-router')(app);
 
-
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-  });
+});
 
